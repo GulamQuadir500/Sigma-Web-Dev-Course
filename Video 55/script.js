48734de8abf84f7fab4c63b1e1d33a148229f537
GulamQuadir500 <gulamquadir500@gmail.com>
2024-07-12 17:22:26 +0530

    JavaScript Variables, Data Types & Objects

---
+++ Video 55/script.js
@@ -0,0 +1,40 @@
+console.log("Hey this is video 55")
+
+let a = 5;
+let b = 6;
+let c = "Tipu"
+let _a = "Raju"
+// var 55a = "Rohan" // Not Allowed
+
+// console.log(a + b);
+// console.log(typeof a, typeof b, typeof c);
+
+{
+    let a = 66;
+    console.log(a);
+}
+
+console.log(a);
+const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+// Primitive Data type
+let x = "tipu";
+let y = 22;
+let z = 3.55
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+// Object
+let o = {
+    name : "Tipu",
+    "Job Code": 5600,
+    isHandsome: true
+}
+
+console.log(o);
+o.salary = "100 crores";
+console.log(o);
