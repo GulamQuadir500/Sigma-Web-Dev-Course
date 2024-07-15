f658e0feed8060a70f717e1254b7d4469c3c0135
GulamQuadir500 <gulamquadir500@gmail.com>
2024-07-15 16:11:27 +0530

    JavaScript Strings

---
+++ Video 60/index.js
@@ -0,0 +1,28 @@
+console.log("this is strings tutorial");
+
+let a = "Gulam";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length);
+
+let name = "Gulam";
+let freind = "Tipu";
+
+console.log("His name is " + name + " and his friend name is " + freind);
+console.log(`His name is ${name} and his friend's name is ${freind}`);
+
+let b = "Quadir";
+console.log(b.toLocaleUpperCase());
+console.log(b.slice(1, 4));
+console.log(b.slice(1));
+console.log(b.replace("Qu", "11"));
+console.log(b.concat(a, "Parwez", "Tipu"));
+
+let c = "    kala jadu"
+console.log(c);
+console.log(c.trim());
