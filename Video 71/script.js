09cc8b81d929ef7a16dafbbfa1cdb32b1a11f5cd
GulamQuadir500 <gulamquadir500@gmail.com>
2024-07-18 23:53:32 +0530

    Inserting and Removing Elements using JavaScript

---
+++ Video 71/script.js
@@ -0,0 +1,7 @@
+// let div = document.createElement("div");
+// div.innerHTML = "I have been created <b>by tipu</b>"
+// div.setAttribute("class", "created");
+// document.querySelector(".container").append(div)
+
+let cont = document.querySelector(".container");
+cont.insertAdjacentHTML("afterbegin", "<b>I am under the water please help me.</b>");
